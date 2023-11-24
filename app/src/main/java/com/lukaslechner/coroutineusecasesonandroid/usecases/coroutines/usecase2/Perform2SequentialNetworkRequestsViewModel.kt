9872a1daad10d3318b40f141454ed7ca6f91b7c6
Maksymilian Bankowski <maksymilian.bankowski@amistad.pl>
2023-11-24 23:41:23 +0100

    Network call with Coroutines

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,13 +1,28 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+
+
+        viewModelScope.launch {
+            try {
+                val mostRecentVersion = mockApi.getRecentAndroidVersions().last()
+                val versionFeatures = mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+                uiState.value = UiState.Success(versionFeatures)
+            }catch (e: Exception){
+                uiState.value = UiState.Error(e.message ?: "Network request failed")
+            }
+
+        }
         // TODO: Exercise 1
         // switch to branch "coroutine_course_full" to see solution
     }
